943b977c5fe7d7325dff61c01b6f239e284531ba
Pr1nkos <prinkos1@gmail.com>
2025-04-22 19:21:04 +0300

    (fix) Sqlite запрос

--- wishes/-- SQLite.sql
+++ wishes/-- SQLite.sql
@@ -1,3 +1,3 @@
 ﻿-- SQLite
-INSERT INTO auth_user (password, last_login, is_superuser, username, last_name, email, is_staff, is_active, date_joined, first_name)
-VALUES ('pbkdf2_sha256$870000$EuNV9Ac1YTlwAFo99HPSUG$RAcjWBw8SWuUpMgqNeD69IwPOvQ+fsOfUjBgo2C4I1c=', '2025-04-08 16:33:52.485247', 0, 'test1', '', 'test2@test.com', 0, 1, '2025-04-08 16:12:00.429696', 0);
+SELECT id, user_id, group_id
+FROM auth_user_groups;
